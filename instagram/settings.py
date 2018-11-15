b5d7f66dc78b492c84c083b38d046af79d0f5e5c
Gift-Lumumba <giftlumumba2@gmail.com>
2018-11-15 10:29:49 +0300

    added some files

--- instagram/settings.py
+++ instagram/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'images',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -63,10 +64,12 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'instagram.wsgi.application'
 
@@ -76,10 +79,13 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallary',
+        'USER': 'moringaschool',
+        'PASSWORD':'21336622',
     }
 }
+
 
 
 # Password validation
