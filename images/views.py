68c390174cca8f1501d12e6b239aca4d407c5f3f
Ekiru Timothy <ekiru@outlook.com>
2018-11-16 00:12:28 +0300

    sorted the urls

--- images/views.py
+++ images/views.py
@@ -80,7 +80,7 @@ def search_results(request):
     if 'profile' in request.GET and request.GET["profile"]:
         search_term = request.GET.get("profile")
         searched_profile = Profile.search_by_username(search_term)
-        message = f"{search_term}"
+        message = f'{search_term}'
 
         return render(request, 'insta/search.html',{"message":message,"profiles": searched_profile})
 
