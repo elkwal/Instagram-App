1311c4779038a8772addd92937eeee48bf76d5cb
Ekiru Timothy <ekiru@outlook.com>
2018-11-15 23:41:37 +0300

    commit changes made

--- instagram/settings.py
+++ instagram/settings.py
@@ -132,4 +132,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+
+# configuring the location for media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
