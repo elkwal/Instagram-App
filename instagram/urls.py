4fda248fc7d1422475d109324a66d2e01c000525
Ekiru Timothy <ekiru@outlook.com>
2018-11-16 00:08:45 +0300

    sorted the urls

--- instagram/urls.py
+++ instagram/urls.py
@@ -18,7 +18,7 @@ from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^',include('insta.urls')),
+    url(r'^',include('images.urls')),
     # url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^accounts/', include('registration.backends.hmac.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}),
