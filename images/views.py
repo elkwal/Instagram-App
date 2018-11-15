9d4801e8df24cb6c6baf86dc8442365fec3e7b82
Gift-Lumumba <giftlumumba2@gmail.com>
2018-11-15 17:12:30 +0300

    made some changes

---
+++ images/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import 
+
+
+#Create your views here
+def index(request):
+    title = 'Welcome to the most amazing social media app clone'
+    return render(request, 'index.html',{"title": title})
--- instagram/urls.py
+++ instagram/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page":'/'}),
-    url(r'^login/$', views.login, {}),
+    url(r'^login/$', views.login, {})
 ]
